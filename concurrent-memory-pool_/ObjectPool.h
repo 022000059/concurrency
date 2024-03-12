6913ded9b50f2d508feb14c4a7befd4e46e39f05
ying_tu <2663518092@qq.com>
2024-03-12 14:15:56 +0800

    提交文件

--- concurrent-memory-pool_/ObjectPool.h
+++ concurrent-memory-pool_/ObjectPool.h
@@ -2,7 +2,9 @@
 #pragma once
 #pragma once
 #include<iostream>
+#include<vector>
 #include <new>
+#include<time.h>
 using std::cout;
 using std::endl;
 using std::bad_alloc;
@@ -18,7 +20,7 @@ public:
 		if (_freeList)
 		{//优先使用还回来的内存块
 			void* next = *((void **)_freeList);//	取freelist的头4或8字节
-			obj = _freeList;
+			obj =(T*) _freeList;
 			_freeList = next;//定长内存池，所以每次取得大小一样
 
 
@@ -30,7 +32,8 @@ public:
 			//剩余内存不够一个对象大小的时候，则重新开大块新空间
 			if (_remainBytes < sizeof(T))
 			{
-				_memory = (char*)malloc(128 * 1024);
+				_remainBytes= 128 * 1024;
+				_memory = (char*)malloc(_remainBytes);
 				if (_memory == nullptr)
 				{
 					throw bad_alloc();
@@ -63,7 +66,7 @@ public:
 		//else
 		//{
 			//头插法把obj插入_freelist
-		obj->～T();
+		obj->~T();
 		*(void **)obj = _freeList;
 		_freeList = obj;
 		//}
@@ -75,3 +78,66 @@ private:
 	void* _freeList = nullptr;//换回来过程中链接的自由链表的头指针
 
 };
+
+struct TreeNode
+{
+	int _val;
+	TreeNode* _left;
+	TreeNode* _right;
+
+	TreeNode()
+		:_val(0)
+		, _left(nullptr)
+		, _right(nullptr)
+	{}
+};
+
+void TestObjectPool()
+{
+	// 申请释放的轮次
+	const size_t Rounds = 5;
+
+	// 每轮申请释放多少次
+	const size_t N = 100000;
+
+	std::vector<TreeNode*> v1;
+	v1.reserve(N);
+
+	size_t begin1 = clock();
+	for (size_t j = 0; j < Rounds; ++j)
+	{
+		for (int i = 0; i < N; ++i)
+		{
+			v1.push_back(new TreeNode);
+		}
+		for (int i = 0; i < N; ++i)
+		{
+			delete v1[i];
+		}
+		v1.clear();
+	}
+
+	size_t end1 = clock();
+
+	std::vector<TreeNode*> v2;
+	v2.reserve(N);
+
+	ObjectPool<TreeNode> TNPool;
+	size_t begin2 = clock();
+	for (size_t j = 0; j < Rounds; ++j)
+	{
+		for (int i = 0; i < N; ++i)
+		{
+			v2.push_back(TNPool.New());
+		}
+		for (int i = 0; i < N; ++i)
+		{
+			TNPool.Delete(v2[i]);
+		}
+		v2.clear();
+	}
+	size_t end2 = clock();
+
+	cout << "new cost time:" << end1 - begin1 << endl;
+	cout << "object pool cost time:" << end2 - begin2 << endl;
+}
